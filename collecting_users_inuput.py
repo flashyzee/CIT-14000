89438262136ad86a1d0e8384345596c6ff55c4b1
Zion <134931509+flashyzee@users.noreply.github.com>
2024-02-01 00:05:12 -0500

    Add files via upload
    This was my first Python script and I used it to practice collecting, formatting, and outputting messages to the user.

---
+++ collecting_users_inuput.py
@@ -0,0 +1,43 @@
+# question 1
+print ("Hello World")
+
+#question 2
+num = 13 
+
+#question 3 
+print(num)
+
+#question 4
+print(f"My number is {num}")
+
+#question 5
+first_name = input("Enter your first name: ")
+
+#question 6 
+print (first_name)
+
+#question 7 
+print (f"Hello, World! My name is {first_name}!")
+ 
+ #question 8
+birth_year = input("Enter your birth_year: ")
+
+#question 9: Finding users age by subtracting 
+age = 2024 - int(birth_year)
+ 
+#question 10
+print (f"Hello, World! My name is {first_name} and Iam approximately {age} years old.")
+
+#question 11 Calcuate how many months the user has lived 
+lived_month = age * 12 
+print(f"Hello, World! My name is {first_name} and I have lived {lived_month} months.")
+  
+#question 12 Calculate how may days the user has lived 
+lived_day = lived_month * 30 
+print(f"Hello, World! My name is {first_name} and I have lived {lived_day} days.")
+
+#question 13 Calculating the year the users will retire 
+retirement_age = 65
+retirement_year = int(birth_year) + retirement_age
+
+print(f"The user will retire at age 65 in year {retirement_year}.")
